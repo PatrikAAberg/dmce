f1d21ef94fd74556acba04b455f9780e38187e61
Magnus Templing <magnus.templing@ericsson.com>
2021-01-20 16:44:20 +0100

    t: gcc.c-torture: skip time/uptime

--- test/gcc.c-torture/gcc.c-torture.sh
+++ test/gcc.c-torture/gcc.c-torture.sh
@@ -3,13 +3,10 @@
 set -e
 
 gcc_version=6.3.0
-
-# change the output format for the built in bash command 'time'
 PROG_NAME=$(basename $0 .sh)
-TIMEFORMAT="${PROG_NAME}: real: %3lR user: %3lU sys: %3lS"
 
 function _echo() {
-	echo $(date '+%Y-%m-%d %H:%M:%S'):${PROG_NAME}:$@:$(uptime)
+	echo $(date '+%Y-%m-%d %H:%M:%S'):${PROG_NAME}:$@
 }
 
 # DMCE work directory
@@ -28,101 +25,90 @@ dmce_exec_path="$(git rev-parse --show-toplevel 2> /dev/null)"
 if [ $? -ne 0 ]; then
 	set -e
 	if ! [ -e dmce/.git ]; then
-		time {
-			_echo "fetch DMCE"
-			set -x
-			git -C ${my_work_path} clone --depth 1 https://github.com/PatrikAAberg/dmce.git
-			{ set +x; } 2>/dev/null
-		}
+		_echo "fetch DMCE"
+		set -x
+		git -C ${my_work_path} clone --depth 1 https://github.com/PatrikAAberg/dmce.git
+		{ set +x; } 2>/dev/null
 	fi
 	dmce_exec_path=${PWD}/dmce
 fi
 set -e
 
-pushd ${my_work_path}
+pushd ${my_work_path} &> /dev/null
 
 if [ ! -e "gcc-${gcc_version}.tar.bz2" ]; then
-	time {
-		_echo "fetch GCC"
-		set -x
-		wget -q ftp://ftp.gnu.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.bz2
-		{ set +x; } 2>/dev/null
-	}
+	_echo "fetch GCC"
+	set -x
+	wget -q ftp://ftp.gnu.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.bz2
+	{ set +x; } 2>/dev/null
 fi
 
-time {
-	_echo "unpack GCC"
-	set -x
-	tar -C ${my_work_path} -xf gcc-${gcc_version}.tar.bz2 gcc-${gcc_version}/gcc/testsuite/${PROG_NAME}
-	{ set +x; } 2>/dev/null
-}
+_echo "unpack GCC"
+set -x
+tar -C ${my_work_path} -xf gcc-${gcc_version}.tar.bz2 gcc-${gcc_version}/gcc/testsuite/${PROG_NAME}
+{ set +x; } 2>/dev/null
 
-time {
-	_echo "create git"
-	set -x
-	cd gcc-${gcc_version}/gcc/testsuite/${PROG_NAME}
+_echo "create git"
+set -x
+cd gcc-${gcc_version}/gcc/testsuite/${PROG_NAME}
 
-	git init
-	git commit -m "empty" --allow-empty
-	git add .
-	git commit -m "initial commit"
+git init
+git commit -m "empty" --allow-empty
+git add .
+git commit -m "initial commit"
 
-	# gcc version or standard diffs, we are not that picky for this usage
-	git rm compile/20011119-1.c
-	git rm compile/20000120-2.c
-	git rm compile/20011119-2.c
-	git rm compile/20030305-1.c
-	git rm compile/20050215-1.c
-	git rm compile/20050215-2.c
-	git rm compile/20050215-3.c
-	git rm compile/20111209-1.c
-	git rm compile/20160205-1.c
-	git rm compile/920520-1.c
-	git rm compile/920521-1.c
-	git rm compile/dll.c
-	git rm compile/limits-*.c
-	git rm compile/mipscop-1.c
-	git rm compile/mipscop-2.c
-	git rm compile/mipscop-3.c
-	git rm compile/mipscop-4.c
-	git rm compile/pr37669.c
-	git rm compile/pr46534.c
-	git rm compile/pr67143.c
-	git rm compile/pr70199.c
-	git rm compile/pr70633.c
-	git rm execute/20000227-1.c
-	git rm execute/20010206-1.c
-	git rm execute/fprintf-chk-1.c
-	git rm execute/pr19449.c
-	git rm execute/pr67714.c
-	git rm execute/pr68532.c
-	git rm execute/pr71494.c
-	git rm execute/printf-chk-1.c
-	git rm execute/vfprintf-chk-1.c
-	git rm execute/vprintf-chk-1.c
-	git rm unsorted/dump-noaddr.c
-	git rm compile/20001226-1.c
-	git commit -m "broken"
+# gcc version or standard diffs, we are not that picky for this usage
+git rm compile/20011119-1.c
+git rm compile/20000120-2.c
+git rm compile/20011119-2.c
+git rm compile/20030305-1.c
+git rm compile/20050215-1.c
+git rm compile/20050215-2.c
+git rm compile/20050215-3.c
+git rm compile/20111209-1.c
+git rm compile/20160205-1.c
+git rm compile/920520-1.c
+git rm compile/920521-1.c
+git rm compile/dll.c
+git rm compile/limits-*.c
+git rm compile/mipscop-1.c
+git rm compile/mipscop-2.c
+git rm compile/mipscop-3.c
+git rm compile/mipscop-4.c
+git rm compile/pr37669.c
+git rm compile/pr46534.c
+git rm compile/pr67143.c
+git rm compile/pr70199.c
+git rm compile/pr70633.c
+git rm execute/20000227-1.c
+git rm execute/20010206-1.c
+git rm execute/fprintf-chk-1.c
+git rm execute/pr19449.c
+git rm execute/pr67714.c
+git rm execute/pr68532.c
+git rm execute/pr71494.c
+git rm execute/printf-chk-1.c
+git rm execute/vfprintf-chk-1.c
+git rm execute/vprintf-chk-1.c
+git rm unsorted/dump-noaddr.c
+git rm compile/20001226-1.c
+git commit -m "broken"
 
-	# add DMCE config and update paths
-	cp -v ${dmce_exec_path}/test/${PROG_NAME}/dmceconfig .dmceconfig
-	sed -i "s|DMCE_EXEC_PATH:.*|DMCE_EXEC_PATH:${dmce_exec_path}|" .dmceconfig
-	sed -i "s|DMCE_CONFIG_PATH:.*|DMCE_CONFIG_PATH:${my_test_path}|" .dmceconfig
-	sed -i "s|DMCE_CMD_LOOKUP_HOOK:.*|DMCE_CMD_LOOKUP_HOOK:${my_test_path}/cmdlookuphook.sh|" .dmceconfig
-	sed -i "s|DMCE_PROBE_SOURCE:.*|DMCE_PROBE_SOURCE:${my_test_path}/dmce-probe-monolith.c|" .dmceconfig
-	git add .dmceconfig
-	git commit -m "DMCE config"
-	{ set +x; } 2>/dev/null
-}
+# add DMCE config and update paths
+cp -v ${dmce_exec_path}/test/${PROG_NAME}/dmceconfig .dmceconfig
+sed -i "s|DMCE_EXEC_PATH:.*|DMCE_EXEC_PATH:${dmce_exec_path}|" .dmceconfig
+sed -i "s|DMCE_CONFIG_PATH:.*|DMCE_CONFIG_PATH:${my_test_path}|" .dmceconfig
+sed -i "s|DMCE_CMD_LOOKUP_HOOK:.*|DMCE_CMD_LOOKUP_HOOK:${my_test_path}/cmdlookuphook.sh|" .dmceconfig
+sed -i "s|DMCE_PROBE_SOURCE:.*|DMCE_PROBE_SOURCE:${my_test_path}/dmce-probe-monolith.c|" .dmceconfig
+git add .dmceconfig
+git commit -m "DMCE config"
+{ set +x; } 2>/dev/null
 
-time {
-	_echo "launch DMCE"
-	set -x
-	${dmce_exec_path}/dmce-launcher -n $(git rev-list --all --count)
-	{ set +x; } 2>/dev/null
-}
+_echo "launch DMCE"
+set -x
+${dmce_exec_path}/dmce-launcher -n $(git rev-list --all --count)
+{ set +x; } 2>/dev/null
 
-time {
 	_echo "compile"
 	> ${my_work_path}/compile-errors
 	find -name '*.err' -exec rm {} \;
@@ -132,14 +118,13 @@ time {
 				echo ${f} >> ${my_work_path}/compile-errors;
 			fi
 		} &
-	done
+done
 
-	wait
+wait
 
-	set -x
-	find -name '*.err' -type f ! -size 0 -exec cat {} \;
-	{ set +x; } 2>/dev/null
-}
+set -x
+find -name '*.err' -type f ! -size 0 -exec cat {} \;
+{ set +x; } 2>/dev/null
 
 _echo "results"
 set -x
