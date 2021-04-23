dc6f060a3432f0f7fd4b51d256316af6fffba322
Patrik Aberg <patrik.aberg@ericsson.com>
2021-04-23 15:22:03 +0200

    probes: atexit and stderr

---
+++ dmce-probe-user-atexit.c
@@ -0,0 +1,48 @@
+/* This is a simple template for a linux userspace probe using the printf on stderr  */
+#ifndef __DMCE_PROBE_FUNCTION_BODY__
+#define __DMCE_PROBE_FUNCTION_BODY__
+
+#include <stdio.h>
+#include <stdlib.h>
+#define DMCE_NUM_PROBES 100000
+
+static uint32_t dmce_buffer[DMCE_NUM_PROBES] = {0};
+static uint32_t dmce_tmp_buffer[DMCE_NUM_PROBES] = {0};
+static int registered_at_exit = 0;
+
+static void dmce_atexit(void){
+
+    FILE *fp;
+    if (!(fp = fopen("/tmp/dmcebuffer.bin", "r"))) {
+
+        fp = fopen("/tmp/dmcebuffer.bin", "w");
+        fwrite(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+        fclose(fp);
+
+        fp = fopen("/tmp/dmcebuffer.bin", "r"); 
+    }
+
+    size_t n = fread(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+    if (n != (4 * DMCE_NUM_PROBES))
+        printf("DMCE: Something went terribly wrong...\n");
+
+    for (int i = 0; i < DMCE_NUM_PROBES; i++)
+        dmce_tmp_buffer[i] += dmce_buffer[i];
+    fclose(fp);
+
+    fp = fopen("/tmp/dmcebuffer.bin", "w");
+    fwrite(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+    fclose(fp);
+}
+
+static void dmce_probe_body(unsigned int probenbr) {
+
+    if (!registered_at_exit) {
+
+        atexit(dmce_atexit);
+        registered_at_exit = 1;
+    }
+
+    dmce_buffer[probenbr] ++;
+}
+#endif //__DMCE_PROBE_FUNCTION_BODY__
---
+++ dmce-probe-user-stderr.c
@@ -0,0 +1,11 @@
+/* This is a simple template for a linux userspace probe using the printf on stderr  */
+#ifndef __DMCE_PROBE_FUNCTION_BODY__
+#define __DMCE_PROBE_FUNCTION_BODY__
+
+#include <stdio.h>
+
+static void dmce_probe_body(unsigned int probenbr)
+{
+    fprintf(stderr, "\nDMCE_PROBE(%u)\n ",probenbr);
+}
+#endif //__DMCE_PROBE_FUNCTION_BODY__
