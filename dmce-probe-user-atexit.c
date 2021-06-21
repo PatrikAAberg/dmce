e92c893eb6e920da68f797af4362f01f54bd41c4
Patrik Aberg <patrik.aberg@ericsson.com>
2021-06-21 22:25:50 +0200

    probes: error check for atexit probe

--- dmce-probe-user-atexit.c
+++ dmce-probe-user-atexit.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #define DMCE_NUM_PROBES 100000
 
 static uint32_t dmce_buffer[DMCE_NUM_PROBES] = {0};
@@ -16,13 +17,13 @@ static void dmce_atexit(void){
     if (!(fp = fopen("/tmp/dmcebuffer.bin", "r"))) {
 
         fp = fopen("/tmp/dmcebuffer.bin", "w");
-        fwrite(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+        fwrite(dmce_tmp_buffer, sizeof(uint32_t), DMCE_NUM_PROBES, fp);
         fclose(fp);
 
         fp = fopen("/tmp/dmcebuffer.bin", "r"); 
     }
 
-    size_t n = fread(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+    size_t n = fread(dmce_tmp_buffer, sizeof(uint32_t), DMCE_NUM_PROBES, fp);
     if (n != DMCE_NUM_PROBES)
         printf("DMCE: Something went terribly wrong...\n");
 
@@ -31,7 +32,7 @@ static void dmce_atexit(void){
     fclose(fp);
 
     fp = fopen("/tmp/dmcebuffer.bin", "w");
-    fwrite(dmce_tmp_buffer, DMCE_NUM_PROBES, 4, fp);
+    fwrite(dmce_tmp_buffer, sizeof(uint32_t), DMCE_NUM_PROBES, fp);
     fclose(fp);
 }
 
