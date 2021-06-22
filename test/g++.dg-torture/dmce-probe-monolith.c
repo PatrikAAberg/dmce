752515cc07e23a402c8711fe898891410542bbb4
Patrik Aberg <patrik.aberg@ericsson.com>
2021-06-22 17:28:03 +0200

    test: C++: prolog from file

--- test/g++.dg-torture/dmce-probe-monolith.c
+++ test/g++.dg-torture/dmce-probe-monolith.c
@@ -1,9 +1,8 @@
-/* This is a simple template for a DMCE probe in a statically linked monolitic system              */
-/* NOTE! the array dmce_probes[] array must be defined somewhere in the build and zero-initalized! */
+/* Test probe for code trace only */
 #ifndef __DMCE_PROBE_FUNCTION_BODY__
 #define __DMCE_PROBE_FUNCTION_BODY__
 
-extern int dmce_probes[5000];
+extern int dmce_probes[30000];
 
 static void dmce_probe_body(unsigned int probenbr)
 {
