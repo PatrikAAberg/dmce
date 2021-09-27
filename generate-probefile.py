aeba5ba71373ed0d15cd7ae68432a83bf2fd0d49
Patrik Aberg <patrik.aberg@ericsson.com>
2021-09-27 11:43:21 +0200

    parser: size_t for C

--- generate-probefile.py
+++ generate-probefile.py
@@ -323,11 +323,13 @@ re_sections_parmdecl.append(re.compile(r'.*-ParmVarDecl Hexnumber.*'))
 
 re_parmdeclarations = []
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'size_t\' cinit.*'))
+re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'size_t\':\'unsigned long\' cinit.*'))
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'long\' cinit.*'))
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'unsigned long\' cinit.*'))
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'int\' cinit.*'))
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'unsigned int\' cinit.*'))
 re_parmdeclarations.append(re.compile(r'.*-VarDecl Hexnumber.*used\s(\S*)\s\'.* \*\' cinit.*'))
+re_parmdeclarations.append(re.compile(r'.*-ParmVarDecl Hexnumber.*used\s(\S*)\s\'size_t\':\'unsigned long\'.*'))
 re_parmdeclarations.append(re.compile(r'.*-ParmVarDecl Hexnumber.*used\s(\S*)\s\'size_t\'.*'))
 re_parmdeclarations.append(re.compile(r'.*-ParmVarDecl Hexnumber.*used\s(\S*)\s\'long\'.*'))
 re_parmdeclarations.append(re.compile(r'.*-ParmVarDecl Hexnumber.*used\s(\S*)\s\'unsigned long\'.*'))
