c23584a847b5ee1e728f650997f0dd67bca453e2
Patrik Aberg <patrik.aberg@ericsson.com>
2021-10-04 11:28:05 +0200

    trace: multi-thread support

--- dmce-probe-trace-atexit-D5-CB.c
+++ dmce-probe-trace-atexit-D5-CB.c
@@ -1,28 +1,29 @@
 /* This is a simple template for a linux userspace probe using the printf on stderr  */
 #ifndef __DMCE_PROBE_FUNCTION_BODY__
 #define __DMCE_PROBE_FUNCTION_BODY__
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#define DMCE_MAX_HITS 100000
+#define DMCE_MAX_HITS 10000
 
 typedef struct {
 
     uint64_t timestamp;
     uint64_t probenbr;
     uint64_t vars[5];
+    uint64_t cpu;
 } dmce_probe_entry_t;
 
 #ifdef __cplusplus
 static dmce_probe_entry_t* dmce_buf_p = nullptr;
-static int* dmce_probe_hitcount_p = nullptr;
+static unsigned int* dmce_probe_hitcount_p = nullptr;
 #else
-static dmce_probe_entry_t* dmce_buf_p;
-static int* dmce_probe_hitcount_p = 0;
+static dmce_probe_entry_t* dmce_buf_p = 0;
+static unsigned int* dmce_probe_hitcount_p = 0;
 #endif
+static int dmce_buffer_setup_done = 0;
 static __inline__ uint64_t dmce_tsc(void) {
 
 #if defined(__x86_64__)
@@ -45,6 +46,11 @@ static void dmce_atexit(void) {
     remove("/tmp/dmce-trace-buffer-lock");
 }
 
+#ifdef _GNU_SOURCE
+#include <sched.h>
+#else
+int getcpu(unsigned int *cpu, unsigned int *node);
+#endif
 static void dmce_probe_body(unsigned int dmce_probenbr,
                             uint64_t dmce_param_a,
                             uint64_t dmce_param_b,
@@ -53,7 +59,6 @@ static void dmce_probe_body(unsigned int dmce_probenbr,
                             uint64_t dmce_param_e) {
 
     /* Set up buffer and control if not done */
-
 #ifdef __cplusplus
     if (dmce_trace_enabled_p == nullptr)
 #else 
@@ -65,40 +70,42 @@ static void dmce_probe_body(unsigned int dmce_probenbr,
         /* If first time: allocate buffer, init env var and set up exit hook */
         /* TODO: Make this less racy maybe, but only a prooblem if threads spawned before the first probe */
         /* env var format: enabled buf_p hitcount*/
-        if (! (mkdir("/tmp/dmce-trace-buffer-lock",0))) {
-        
-            char s[32 * 3];
-
-            dmce_buf_p = (dmce_probe_entry_t*)calloc( DMCE_MAX_HITS + 10,   /* room for race until we introduce a lock */
-                                                      sizeof(dmce_probe_entry_t));
+        if (! dmce_buffer_setup_done) {
+            if (! (mkdir("/tmp/dmce-trace-buffer-lock",0))) {
             
-            dmce_trace_enabled_p = (int*)calloc(1, sizeof(int));
-            dmce_probe_hitcount_p = (int*)calloc(1, sizeof(int));
+                char s[32 * 3];
+                dmce_buf_p = (dmce_probe_entry_t*)calloc( DMCE_MAX_HITS + 10,   /* room for race until we introduce a lock */
+                                                          sizeof(dmce_probe_entry_t));
 
-            sprintf(s, "%p %p %p", dmce_trace_enabled_p, dmce_buf_p, dmce_probe_hitcount_p);
-            setenv("dmce_trace_control", s, 0);
+                dmce_trace_enabled_p = (int*)calloc(1, sizeof(int));
+                dmce_probe_hitcount_p = (unsigned int*)calloc(1, sizeof(int));
 
-            atexit(dmce_atexit);
+                sprintf(s, "%p %p %p", dmce_trace_enabled_p, dmce_buf_p, dmce_probe_hitcount_p);
+                setenv("dmce_trace_control", s, 0);
 
-            /* Just to avoid unused-function warnings */
-            dmce_trace_disable();
-            dmce_trace_enable();
+                atexit(dmce_atexit);
+
+                /* Just to avoid unused-function warnings */
+                dmce_trace_disable();
+                dmce_trace_enable();
+            }
+            else {
+               /* Buffer already exist, wait for env var to be available and only init local pointers */
+               while (NULL == (s_control_p  = getenv("dmce_trace_control")));
+               sscanf(s_control_p, "%p %p %p", &dmce_trace_enabled_p, &dmce_buf_p, &dmce_probe_hitcount_p);
+            }
+            dmce_buffer_setup_done = 1;
         }
-        else {
-
-           /* Buffer already exist, wait for env var to be available and only init local pointers */
-           while (! (s_control_p  = getenv("dmce_trace_control")));
-           sscanf(s_control_p, "%p %p %p", &dmce_trace_enabled_p, &dmce_buf_p, &dmce_probe_hitcount_p);
-        }
-
     }
-
     if (dmce_trace_is_enabled() && *dmce_probe_hitcount_p < DMCE_MAX_HITS) {
 
+        unsigned int cpu;
         __atomic_fetch_add (dmce_probe_hitcount_p, 1, __ATOMIC_SEQ_CST);
         dmce_probe_entry_t* e_p = &dmce_buf_p[(*dmce_probe_hitcount_p) - 1];
+        getcpu(&cpu, 0);
         e_p->timestamp = dmce_tsc();
         e_p->probenbr = dmce_probenbr;
+        e_p->cpu = cpu;
         e_p->vars[0] = dmce_param_a;
         e_p->vars[1] = dmce_param_b;
         e_p->vars[2] = dmce_param_c;
--- dmce-prolog-trace-D5.c
+++ dmce-prolog-trace-D5.c
@@ -1,7 +1,6 @@
 /* Test prolog for code trace and data trace with 5 parameters */
 #ifndef __DMCE_PROBE_FUNCTION__HEADER__
 #define __DMCE_PROBE_FUNCTION__HEADER__
-#include <stdint.h>
 #ifdef __cplusplus
 static int* dmce_trace_enabled_p = nullptr;
 #else
@@ -10,6 +9,7 @@ static int* dmce_trace_enabled_p = 0;
 static inline int dmce_trace_is_enabled() { return *dmce_trace_enabled_p; }
 static inline void dmce_trace_enable(void) { *dmce_trace_enabled_p = 1; }
 static inline void dmce_trace_disable(void) { *dmce_trace_enabled_p = 0; }
+typedef unsigned long uint64_t;
 static void dmce_probe_body(unsigned int dmce_probenbr, uint64_t dmce_param_a, uint64_t dmce_param_b, uint64_t dmce_param_c, uint64_t dmce_param_d, uint64_t dmce_param_e);
 #define DMCE_PROBE(dmce_probenbr, dmce_param_a, dmce_param_b, dmce_param_c, dmce_param_d, dmce_param_e) (dmce_probe_body(dmce_probenbr, dmce_param_a, dmce_param_b, dmce_param_c, dmce_param_d, dmce_param_e))
 #endif
