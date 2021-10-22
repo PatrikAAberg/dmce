e10b44f952205892344ee39f7fab8a9a86c7d939
Patrik Aberg <patrik.aberg@ericsson.com>
2021-10-22 14:22:16 +0200

    probes: comment cleanup

--- dmce-probe-trace-atexit-D5-SB.c
+++ dmce-probe-trace-atexit-D5-SB.c
@@ -1,4 +1,3 @@
-/* This is a simple template for a linux userspace probe using the printf on stderr  */
 #ifndef __DMCE_PROBE_FUNCTION_BODY__
 #define __DMCE_PROBE_FUNCTION_BODY__
 #include <stdio.h>
