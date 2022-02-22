5e6a0b53ca8b291f7fb5a0fdaecfc9f884c80899
Patrik Aberg <patrik.aberg@ericsson.com>
2022-02-22 17:07:00 +0100

    trace: keep prolog always 20 lines from now

--- probe-examples/dmce-prolog-trace-D5.c
+++ probe-examples/dmce-prolog-trace-D5.c
@@ -13,11 +13,7 @@ typedef unsigned long uint64_t;
 static void dmce_probe_body(unsigned int dmce_probenbr, uint64_t dmce_param_a, uint64_t dmce_param_b, uint64_t dmce_param_c, uint64_t dmce_param_d, uint64_t dmce_param_e);
 #define DMCE_PROBE(dmce_probenbr, dmce_param_a, dmce_param_b, dmce_param_c, dmce_param_d, dmce_param_e) (dmce_probe_body(dmce_probenbr, dmce_param_a, dmce_param_b, dmce_param_c, dmce_param_d, dmce_param_e))
 #endif
-/* 20 lines probe makes counting line number easier to debug */
-/**/
-/**/
-/**/
-/**/
+/* 20 lines prolog makes counting line number easier to debug */
 /**/
 /**/
 /**/
