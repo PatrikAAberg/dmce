a1af1ae6de1a64d01e3e6f608a07fe9d47b06f80
Patrik Aberg <patrik.aberg@ericsson.com>
2022-11-22 17:12:54 +0100

    probes: extern C for coverage probe

--- probe-examples/dmce-prolog-default.c
+++ probe-examples/dmce-prolog-default.c
@@ -1,5 +1,9 @@
 #ifndef __DMCE_PROBE_FUNCTION__HEADER__
 #define __DMCE_PROBE_FUNCTION__HEADER__
+#ifdef __cplusplus
+extern "C" { static void dmce_probe_body(unsigned int probenbr); }
+#else
 static void dmce_probe_body(unsigned int probenbr);
+#endif
 #define DMCE_PROBE(a) (dmce_probe_body(a))
 #endif
