36409436ef4913eabd33b85c533e414df762bb47
Magnus Templing <magnus.templing@ericsson.com>
2023-11-30 14:53:31 +0100

    dmce-set-profile: bash completion

--- dmce-completion.bash
+++ dmce-completion.bash
@@ -29,7 +29,14 @@ _dmce_list_files() {
 	fi
 	git ls-files | \
 		grep -E '\.c$|\.cpp$|\.cc$|\.h$|\.hh$|\.hpp$|\.inc$'
-	}
+}
+
+_dmce_list_profiles() {
+	if ! command -v dmce-set-profile > /dev/null; then
+		return
+	fi
+	dmce-set-profile -l
+}
 
 _dmce() {
 	declare -a opts
@@ -78,13 +85,15 @@ _dmce() {
 
 	case $cur in
 		-*)
-			mapfile -t COMPREPLY  < <(compgen -W "${opts[*]}" -- "$cur")
+			mapfile -t COMPREPLY < <(compgen -W "${opts[*]}" -- "$cur")
+			return
 			;;
 	esac
 
 	case $prev in
 		--file)
 			_filedir
+			return
 			;;
 		--exclude|--include)
 			if [ "${cur: -1}" = "," ]; then
@@ -94,17 +103,107 @@ _dmce() {
 			else
 				f=$(_dmce_list_files)
 			fi
-			mapfile -t COMPREPLY  < <(compgen -W "${f}" -- "$cur")
+			mapfile -t COMPREPLY < <(compgen -W "${f}" -- "$cur")
+			return
 			;;
 		-j)
-			mapfile -t COMPREPLY  < <(compgen -W "$(seq 1 "$(getconf _NPROCESSORS_ONLN)")" -- "$cur")
+			mapfile -t COMPREPLY < <(compgen -W "$(seq 1 "$(getconf _NPROCESSORS_ONLN)")" -- "$cur")
+			return
 			;;
 		-m)
-			mapfile -t COMPREPLY  < <(compgen -W "$(seq 1 100)" -- "$cur")
+			mapfile -t COMPREPLY < <(compgen -W "$(seq 1 100)" -- "$cur")
+			return
 			;;
 		-p|--profile)
-			mapfile -t COMPREPLY  < <(compgen -W "$(command -v dmce-set-profile > /dev/null && dmce-set-profile -l)" -- "$cur")
+			mapfile -t COMPREPLY < <(compgen -W "$(_dmce_list_profiles)" -- "$cur")
+			return
 			;;
 	esac
 }
+
+_dmce_set_profile() {
+	declare -a opts
+	local cur
+	local prev
+
+	opts+=('-c')
+	opts+=('-C')
+	opts+=('--cache')
+	opts+=('--constructs')
+	opts+=('-d')
+	opts+=('-e')
+	opts+=('-E')
+	opts+=('--exclude')
+	opts+=('--excludecapitals')
+	opts+=('-f')
+	opts+=('--file')
+	opts+=('--fixnullptr')
+	opts+=('-h')
+	opts+=('--help')
+	opts+=('-i')
+	opts+=('-I')
+	opts+=('--include')
+	opts+=('--keeppaths')
+	opts+=('-l')
+	opts+=('--listprofiles')
+	opts+=('-m')
+	opts+=('-M')
+	opts+=('--memlimit')
+	opts+=('-n')
+	opts+=('--noderefs')
+	opts+=('--nosignals')
+	opts+=('--notemplates')
+	opts+=('-p')
+	opts+=('-r')
+	opts+=('-s')
+	opts+=('-S')
+	opts+=('--sysincludes')
+	opts+=('-t')
+	opts+=('-T')
+	opts+=('--tentries')
+	opts+=('--toplevel')
+	opts+=('-v')
+	opts+=('--varexclude')
+	opts+=('--varinclude')
+	opts+=('--verbose')
+	opts+=('-y')
+	opts+=('-Y')
+
+	if ! _init_completion -n =; then
+		return
+	fi
+
+	case $cur in
+		-*)
+			mapfile -t COMPREPLY < <(compgen -W "${opts[*]}" -- "$cur")
+			return
+			;;
+	esac
+
+	case $prev in
+		-f|--file)
+			_filedir
+			return
+			;;
+		--exclude|--include)
+			if [ "${cur: -1}" = "," ]; then
+				f=$(_dmce_list_files | sed -e "s|^|${cur}|g" | xargs)
+			elif [[ "${cur}" == *,* ]]; then
+				f=$(_dmce_list_files | sed -e "s|^|${cur%,*},|g" | xargs)
+			else
+				f=$(_dmce_list_files)
+			fi
+			mapfile -t COMPREPLY < <(compgen -W "${f}" -- "$cur")
+			return
+			;;
+		-m|--memlimit)
+			mapfile -t COMPREPLY < <(compgen -W "$(seq 1 100)" -- "$cur")
+			return
+			;;
+	esac
+
+	mapfile -t COMPREPLY < <(compgen -W "$(_dmce_list_profiles)" -- "$cur")
+}
+
 complete -F _dmce dmce
+complete -F _dmce_set_profile dmce-set-profile
